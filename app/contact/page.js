c4cd7209792e324f21132146f82d4750ccbdff23
tydevilish <meawpain12@Gmail.com>
2025-06-18 09:38:51 +0700

    first commit

---
+++ app/contact/page.js
@@ -0,0 +1,16 @@
+export default function Contact() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-600">
+        <img 
+          src="https://i.pinimg.com/736x/b2/60/94/b26094970505bcd59c2e5fe8b6f41cf0.jpg" 
+          alt="Mr. Jirapat Papai" 
+          className="w-64 h-auto rounded-full shadow-lg mb-8 animate-spin"
+        />
+        <h1 className="text-center font-extrabold text-7xl text-green-500 hover:scale-200 duration-500 animate-pulse">
+          Contact Page
+        </h1>
+      </div>
+    </>
+  );
+}
